d6856a11d659f418673046951152a75207a033ac
tnespiderman <katan2304@gmail.com>
2025-06-07 14:45:34 -0400

    woops

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,23 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-
+<HelloWorld msg="Greeting" />
 </template>
 
 <style scoped>
-/* body {
+body {
   background-color: aqua;
 }
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
+
 </style>
--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,21 +1,48 @@
 <script setup>
 import { ref } from 'vue'
 
+
 defineProps({
   msg: String,
 })
-
-const count = ref(0)
 </script>
 
 <template>
-
+<div class="container">
+  <h1>Welcome</h1>
+  <button>Click me!</button>
+</div>
 
   
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+h1{
+  color: #42b883;
+  font-size: 5em;
+  text-align: center;
+  margin-top: 20px;
+  font-family: "Jersey 10", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+button{
+  background-color: #42b883;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 1em;
+  cursor: pointer;
+  font-family: "Jersey 10", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+img {
+  border-radius:12px
 }
 </style>
